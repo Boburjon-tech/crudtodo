3fbad3e08dd36f9b535fde473a8b88b218525316
bobur <bobur@gmail.com>
2025-05-30 22:52:06 +0500

    fixed some errors

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template class="flex items-center ">
   <div class="flex p-60 pl-10 pr-20 lg:pl-110 lg:pr-20  gap-40 ">
-    <div class="flex items-center md:p-10 bg-emerald-900  gap-8 fixed md:top-50 md:left-5 top-0 left-0  ">
-      <input class="p-4 border border-solid" v-model="newWork" placeholder="yangi vazifa">
+    <div class="flex items-center md:p-10   gap-8 fixed md:top-65 md:left-5 top-0 left-0  ">
+      <input class="p-4 rounded-2xl border border-solid" v-model="newWork" placeholder="yangi vazifa">
       <button class="btn btn-primary" @click="addWork">Qo'shish</button>
     </div>
 
     <div class="w-full ">
       <p class="" v-if="works.length===0">Ayni damda ma'lumot yo'q</p>
       <ul class="flex flex-col gap-3 ">
-        <li class="border-2 flex items-center  p-3" v-for="(item,index) in works" :key="index">
+        <li class="border-2 rounded-2xl flex items-center bg-white  p-3" v-for="(item,index) in works" :key="index">
           
-          <p :class={on_true:item.done} class="text-2xl mr-auto text-red-500" @click="makeDone(index)">{{ item.work }}</p>
+          <p :class={on_true:item.done} class="text-2xl mr-auto text-red-600" @click="makeDone(index)">{{ item.work }}</p>
           <button class="mr-3 btn btn-secondary" @click="deleteWork(index)">Delete</button>
         </li>
       </ul>
